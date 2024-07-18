692a8bac7314a37d68d6a7d8a1921483e25c84a6
mau-mv <morm97@hotmail.com>
2024-07-18 13:32:34 -0600

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello, Spring Boot!";
+    }
+}
